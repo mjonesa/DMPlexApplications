2254973b5f6aa68f1408e78333528256c824d3e3
mjonesa <majones2@mdanderson.org>
2014-02-28 15:58:35 -0600

    ENH: Adding in function to determine points inside heated volume

--- tcaSolver.c
+++ tcaSolver.c
@@ -128,8 +128,47 @@ void g3_uu(const PetscScalar u[], const PetscScalar gradU[], const PetscScalar a
   for (d = 0; d < spatialDim; ++d) g3[d*spatialDim+d] = 1.0;
 }
 
-PetscBool StlBoundary(const PetscReal x[])
-  {
+    /* a_tri = b_tri - c_tri */
+/*Read in and pre-process STL file  */
+#define vector(a_tri,b_tri,c_tri) \
+	(a_tri)[0] = (b_tri)[0] - (c_tri)[0];	\
+	(a_tri)[1] = (b_tri)[1] - (c_tri)[1];	\
+	(a_tri)[2] = (b_tri)[2] - (c_tri)[2];
+
+PetscBool StlBoundary(float *p, float *d,
+			float *v0, float *v1, float *v2) {
+
+	float e1[3],e2[3],h[3],s[3],q[3];
+	float a,f,u,v;
+	vector(e1,v1,v0);
+	vector(e2,v2,v0);
+
+	crossProduct(h,d,e2);
+	a = innerProduct(e1,h);
+
+	if (a > -0.00001 && a < 0.00001)
+		return(false);
+
+	f = 1/a;
+	vector(s,p,v0);
+	u = f * (innerProduct(s,h));
+
+	if (u < 0.0 || u > 1.0)
+		return(false);
+
+	crossProduct(q,s,e1);
+	v = f * innerProduct(d,q);
+
+	if (v < 0.0 || u + v > 1.0)
+		return(false);
+
+	// at this stage we can compute t to find out where
+	// the intersection point is on the line
+	t = f * innerProduct(e2,q);
+
+	if (t > 0.00001) // ray intersection
+		return(true);
+
     return PETSC_TRUE;
     return PETSC_FALSE;
   }
